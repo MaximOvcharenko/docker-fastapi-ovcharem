c30cee7e8c3f6c7f21abe19aff494e2857cd9833
MaximOvcharenko <max14102002@hotmail.com>
2026-03-27 11:41:41 +0200

    leson 2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Hello!", "v": "0.1" }
+    return { "msg": "Hello!", "Docker": "0.1" }
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/api/ip")
+async def get_ip(request: Request):
+    return {"ip": request.client.host}
