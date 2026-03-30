00e0bfd5bdc4afd8ff70a3ac576a5d62999880c0
MaximOvcharenko <max14102002@hotmail.com>
2026-03-30 10:00:32 +0300

    aip allows

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware  
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+orgins = ["*"]  # Allow all origins
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=orgins,  # Allow all origins
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 def read_root():
